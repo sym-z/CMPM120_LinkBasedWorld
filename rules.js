ce14b03f3f74002b02f73828566dadd85a30a339
Jack Sims <123803257+sym-z@users.noreply.github.com>
2024-04-11 10:34:32 -0700

    Fixed initial code, can write my story now

--- rules.js
+++ rules.js
@@ -1,22 +1,24 @@
+
 class Start extends Scene {
     create() {
-        this.engine.setTitle("Title goes here"); // TODO: replace this text using this.engine.storyData to find the story title
+        let data = this.engine.storyData;
+        this.engine.setTitle(data.Title); // TODO: replace this text using this.engine.storyData to find the story title
         this.engine.addChoice("Begin the story");
     }
 
     handleChoice() {
-        this.engine.gotoScene(Location, "Home"); // TODO: replace this text by the initial location of the story
+        this.engine.gotoScene(Location, this.engine.storyData.InitialLocation); // TODO: replace this text by the initial location of the story
     }
 }
 
 class Location extends Scene {
     create(key) {
-        let locationData = undefined; // TODO: use `key` to get the data object for the current story location
-        this.engine.show("Body text goes here"); // TODO: replace this text by the Body of the location data
+        let locationData = this.engine.storyData.Locations[key];// TODO: use `key` to get the data object for the current story location
+        this.engine.show(locationData.Body); // TODO: replace this text by the Body of the location data
         
-        if(true) { // TODO: check if the location has any Choices
-            for(let choice of ["example data"]) { // TODO: loop over the location's Choices
-                this.engine.addChoice("action text"); // TODO: use the Text of the choice
+        if(locationData.Choices && locationData.Choices.length > 0) { // TODO: check if the location has any Choices
+            for(let choice of locationData.Choices) { // TODO: loop over the location's Choices
+                this.engine.addChoice(choice.Text, choice); // TODO: use the Text of the choice
                 // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
             }
         } else {
@@ -26,7 +28,7 @@ class Location extends Scene {
 
     handleChoice(choice) {
         if(choice) {
-            this.engine.show("&gt; "+choice.Text);
+            this.engine.show("&gt; "+ choice.Text);
             this.engine.gotoScene(Location, choice.Target);
         } else {
             this.engine.gotoScene(End);
