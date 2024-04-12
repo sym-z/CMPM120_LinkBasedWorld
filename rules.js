3e2ac83841cdc1f9c11009e3790a8f3c5a471f3b
Jack Sims <jdsims@ucsc.edu>
2024-04-12 09:47:42 -0700

    Added new scene for interactive mechanism, have lock and key foundations done

--- rules.js
+++ rules.js
@@ -4,6 +4,7 @@ class Start extends Scene {
         let data = this.engine.storyData;
         this.engine.setTitle(data.Title); 
         this.engine.addChoice("Begin the story");
+        
         // This will be the core of our lock and key mechanism.
         //console.log(this.engine.hasKey);
     }
@@ -19,6 +20,13 @@ class Location extends Scene {
         this.engine.show(locationData.Body);
         
         if(locationData.Choices && locationData.Choices.length > 0) { 
+            if(this.engine.hasKey && locationData.Key && locationData.Key.length > 0)
+            {
+                for(let opt of locationData.Key)
+                {
+                    this.engine.addChoice(opt.Text, opt)
+                }
+            }
             for(let choice of locationData.Choices) { 
                 this.engine.addChoice(choice.Text, choice); 
             }
@@ -28,15 +36,37 @@ class Location extends Scene {
     }
 
     handleChoice(choice) {
-        if(choice) {
+        if(choice.Target == "Hot_Tub")
+        {
+            this.engine.show("&gt; "+ choice.Text);
+            this.engine.gotoScene(Tub);
+            return;
+        }
+        else if(choice.Target == "Key_Get")
+        {
+            this.engine.show("&gt; "+ choice.Text);
+            this.engine.hasKey = true;
+            this.engine.gotoScene(Location, choice.Target);
+        }
+        else if(choice) 
+        {
             this.engine.show("&gt; "+ choice.Text);
             this.engine.gotoScene(Location, choice.Target);
-        } else {
+        } 
+        else 
+        {
             this.engine.gotoScene(End);
         }
     }
 }
-
+// Custom Scene for Location Specific Interactive Mechanism
+class Tub extends Scene
+{
+    create()
+    {
+        this.engine.gotoScene(Location, "Hot_Tub");
+    }
+}
 class End extends Scene {
     create() {
         this.engine.show("<hr>");
